b4597414b1d700815905e4a1bbfb23ecfab5c053
paddog <paddog123@googlemail.com>
2016-04-19 13:25:59 +0200

    Added baisc Player movement
    Added basic player movement collider etc are working.

---
+++ GameFiles/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+    [SerializeField]
+    private float speed;
+
+    private PlayerMotor motor;
+
+	void Start () {
+        motor = GetComponent<PlayerMotor>();
+	}
+	
+	void Update () {
+        float _x = Input.GetAxisRaw("Horizontal");
+        float _y = Input.GetAxisRaw("Vertical");
+
+        Vector2 movVertical = transform.up * _y;
+        Vector2 movHorizontal = transform.right * _x;
+
+        Vector2 velocity = (movVertical + movHorizontal).normalized * speed;
+        Debug.Log(velocity);
+        motor.Move(velocity);
+	}
+}
